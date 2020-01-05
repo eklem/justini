99a27521dbbb1300b962ed1c13ed7c16c90def0f
Espen Klem <espen.klem@gmail.com>
2020-01-05 09:32:45 +0100

    icon examples using yr-icons font

--- weather/index.js
+++ weather/index.js
@@ -3,7 +3,7 @@ fetch('https://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10&lon=9.58&ms
   .then((weatherXML) => {
     let weatherJSON = xmlToJSON.parseString(weatherXML)
     // console.log(JSON.stringify(weatherJSON, null, 2))
-    console.log(JSON.stringify(weatherJSON.weatherdata[0].product[0].time[0], null, 2))
+    console.log(JSON.stringify(weatherJSON.weatherdata[0].product[0].time[10], null, 2))
   })
 /*  testString = '<xml><a>It Works!</a></xml>';    // get some xml (string or document/node)
   result = xmlToJSON.parseString(testString);  // parse
