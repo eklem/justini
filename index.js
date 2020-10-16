ee7ab011acdf8224b4550b29015356690f786435
Espen Klem <espen.klem@gmail.com>
2020-10-16 13:56:20 +0200

    Vue3: Getting data + config Need to clean dataset and define template

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.90&lon=10.79&altitude=49', {
-  headers: {
-    'User-Agent': 'Justini/0.1 github.com/eklem/justini'
-  },
-})
+fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.90&lon=10.79&altitude=49')
   .then(response => response.json())
   .then(data => {
     console.log(JSON.stringify(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code,  null, ' '))
