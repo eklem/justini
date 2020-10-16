599d007b2ff3243aba43779ab95b095f700cbd37
Espen Klem <espen.klem@gmail.com>
2020-10-16 11:10:09 +0200

    Adding yr.icons as a dependencie

--- weather/index.js
+++ weather/index.js
@@ -3,8 +3,8 @@ fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.90&lon=
   .then(data => {
     console.log(JSON.stringify(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code,  null, ' '))
     console.log(JSON.stringify(data.properties.timeseries[0].data.instant.details.wind_speed + ' m/s',  null, ' '))
-    console.log(JSON.stringify(data.properties.timeseries[0].data.instant.details.wind_from_direction + ' degrees',  null, ' '))
-    console.log(JSON.stringify(data.properties.timeseries[0].data.instant.details.air_temperature + ' Celcius',  null, ' '))
+    console.log(JSON.stringify(data.properties.timeseries[0].data.instant.details.wind_from_direction + '°',  null, ' '))
+    console.log(JSON.stringify(data.properties.timeseries[0].data.instant.details.air_temperature + '°C',  null, ' '))
     // console.log(JSON.stringify(data.properties.timeseries[0].data,  null, ' '))
     // console.log(JSON.stringify(weatherJSON.weatherdata[0].product[0].time[10], null, 2))
   })
