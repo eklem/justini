8bff5cae2803eed0f0e5f4b2a0a04e8bc40b2c21
Espen Klem <espen.klem@gmail.com>
2020-10-25 15:47:02 +0100

    on reboot: screenshot + convert to bmp + update screen

---
+++ justini.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd7in5_V2 Demo")
+
+    epd = epd7in5_V2.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+
+    logging.info("3.read bmp file")
+    Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    Himage = Image.open(os.path.join(picdir, 'screenshot.bmp'))
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd7in5.epdconfig.module_exit()
+    exit()
